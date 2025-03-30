6d0dffc1f81be4875df409481245513001b288de
rezzvanov <55750175+rezzvanov@users.noreply.github.com>
2025-03-30 21:36:31 +0300

    feat: create AppDbContext

---
+++ SimpleDataApi/SimpleDataApi/Storage/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleDataApi.Storage
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<CodeValue> CodeValues { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CodeValue>(entity =>
+            {
+                entity.HasKey(e => e.Id)
+                .IsClustered();
+
+                entity.HasIndex(e => new { e.Code, e.Value })
+                .IsClustered(false);
+            });
+        }
+    }
+}
---
+++ SimpleDataApi/SimpleDataApi/Storage/CodeValue.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleDataApi.Storage
+{
+    public class CodeValue
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Value { get; set; }
+
+        public CodeValue(int code, string value)
+        {
+            Code = code;
+            Value = value;
+        }
+    }
+}
