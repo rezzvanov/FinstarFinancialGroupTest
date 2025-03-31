3f1d054fc95a8227967d9eca039999900346ba67
rezzvanov <55750175+rezzvanov@users.noreply.github.com>
2025-03-31 22:41:17 +0300

    feat: create table component

---
+++ codevalues-ui/src/app/table.tsx
@@ -0,0 +1,32 @@
+export type CodeValue = {
+    id: number;
+    code: number;
+    value: string;
+}
+
+export type TableProps = {
+    data: CodeValue[];
+}
+
+export default function Table(props: TableProps) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Code</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.code}</td>
+                        <td>{item.value}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
