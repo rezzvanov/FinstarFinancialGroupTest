c8b8bdcd606c3eb175eabd0c166654d34a58aeed
rezzvanov <55750175+rezzvanov@users.noreply.github.com>
2025-03-31 23:40:50 +0300

    feat: create Filters Panel component

---
+++ codevalues-ui/src/app/filtersPanel.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+type FiltersPanelProps = {
+    onFilter: (filters: Omit<CodeValue, 'id'>) => void;
+};
+
+export type CodeValue = {
+    id: number;
+    code: number;
+    value: string;
+}
+
+export default function FiltersPanel(props: FiltersPanelProps) {
+    const [code, setCode] = useState<string>('');
+    const [value, setValue] = useState<string>('');
+
+    const handleFilter = () => {
+        props.onFilter({
+            code: code ? parseInt(code) : 0,
+            value: value
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                <label>Code:
+                    <input
+                        type="number"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>Value:
+                    <input
+                        type="text"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                    />
+                </label>
+            </div>
+            <button onClick={handleFilter}>Apply Filters</button>
+        </div>
+    );
+}
