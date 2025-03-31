231b0f3afb794b04d2e1d0855a16f6ccc7bdb4d3
rezzvanov <55750175+rezzvanov@users.noreply.github.com>
2025-03-31 21:50:12 +0300

    feat: create initial migration

---
+++ SimpleDataApi/InitialMigration.sql
@@ -0,0 +1,28 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [CodeValues] (
+    [Id] int NOT NULL IDENTITY,
+    [Code] int NOT NULL,
+    [Value] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_CodeValues] PRIMARY KEY CLUSTERED ([Id])
+);
+
+CREATE NONCLUSTERED INDEX [IX_CodeValues_Code] ON [CodeValues] ([Code]);
+
+CREATE NONCLUSTERED INDEX [IX_CodeValues_Value] ON [CodeValues] ([Value]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250331184320_InitialCreate', N'9.0.3');
+
+COMMIT;
+GO
+
---
+++ SimpleDataApi/SimpleDataApi/Migrations/20250331184320_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleDataApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodeValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeValues", x => x.Id)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeValues_Code",
+                table: "CodeValues",
+                column: "Code")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeValues_Value",
+                table: "CodeValues",
+                column: "Value")
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodeValues");
+        }
+    }
+}
